70f6a8ac3de91719f81ba68023486391ea3459bc
Alexander Popiak <alexander.popiak@parity.io>
2020-03-03 11:58:23 +0100

    add test for saturated_mul

--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,13 @@ use sp_runtime::Fixed64;
 pub(crate) fn saturated_mul(r: Fixed64, uint: u64) -> u64 {
     r.saturated_multiply_accumulate(uint).saturating_sub(uint)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+	fn saturated_mul_test() {
+        assert_eq!(saturated_mul(Fixed64::from_rational(15, 10), 100), 150);
+	}
+}
