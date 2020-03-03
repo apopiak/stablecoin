3bb9e9613236664d4c848b24553dd5fe02f98c2a
Alexander Popiak <alexander.popiak@parity.io>
2020-03-03 11:53:20 +0100

    add saturated_mul util function

---
+++ src/utils.rs
@@ -0,0 +1,9 @@
+use sp_runtime::Fixed64;
+
+/// Multiply a rational with an unsigned integer.
+///
+/// Example:
+/// `assert_eq!(saturated_mul(Fixed64::from_rational(15, 10), 100), 150);`
+pub(crate) fn saturated_mul(r: Fixed64, uint: u64) -> u64 {
+    r.saturated_multiply_accumulate(uint).saturating_sub(uint)
+}
